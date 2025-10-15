70e7c77dd90398feedc7de67639875d3ed8b6379
juanpa1808 <breaded08@gmail.com>
2025-10-15 15:48:47 +0000

    eliminar configuracion de react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,9 @@ import icon from 'astro-icon';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()],
+  integrations: [icon()],
 
   vite: {
     plugins: [tailwindcss()]
